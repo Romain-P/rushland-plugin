d380c8ad63e0035003dd4f26ea8c7cc760ea12e4
Blackrush <blackrushx@gmail.com>
2014-10-30 16:38:28 +0100

    create ItemLoader

--- src/main/java/org/rushland/plugin/PluginFactory.java
+++ src/main/java/org/rushland/plugin/PluginFactory.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.rushland.plugin.entities.Client;
 import org.rushland.plugin.entities.Grade;
+import org.rushland.plugin.entities.Item;
 import org.rushland.plugin.enums.PluginType;
 
 import java.util.HashMap;
@@ -19,6 +20,7 @@ public class PluginFactory {
     public final long slots;
     public final Map<String, Client> clients;
     public final Map<Integer, Grade> grades;
+    public final Map<Integer, Item> items;
 
     @Inject
     public PluginFactory(JavaPlugin plugin) {
@@ -26,5 +28,6 @@ public class PluginFactory {
         this.slots = plugin.getConfig().getLong("plugin.slots");
         this.clients = new HashMap<>();
         this.grades = new HashMap<>();
+        this.items = new HashMap<>();
     }
 }
