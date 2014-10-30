22050194db7fe42893453ca085e4c22cc4a742c0
Blackrush <blackrushx@gmail.com>
2014-10-30 16:46:50 +0100

    create method PluginFactory#generateInventory

--- src/main/java/org/rushland/plugin/PluginFactory.java
+++ src/main/java/org/rushland/plugin/PluginFactory.java
@@ -2,6 +2,8 @@ package org.rushland.plugin;
 
 import com.google.inject.Inject;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.rushland.plugin.entities.Client;
 import org.rushland.plugin.entities.Grade;
@@ -9,6 +11,7 @@ import org.rushland.plugin.entities.Item;
 import org.rushland.plugin.enums.PluginType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,5 +32,20 @@ public class PluginFactory {
         this.clients = new HashMap<>();
         this.grades = new HashMap<>();
         this.items = new HashMap<>();
+    }
+
+    /**
+     * Generate a Bukkit Inventory containing all item given as ID.
+     * @param title resulting inventory's title
+     * @param ids all items' id which has be present in resulting inventory
+     * @return a non-null inventory
+     */
+    public Inventory generateInventory(String title, List<Integer> ids) {
+        Inventory result = Bukkit.createInventory(null, (ids.size() % 9 + 1) * 9, title);
+        for (Integer id : ids) {
+            Item item = items.get(id);
+            result.addItem(item.generate());
+        }
+        return result;
     }
 }
