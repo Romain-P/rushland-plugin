08183c292a046c967766cabc8a7ea0dc4e32ed4b
Romain <skryn01@gmail.com>
2014-10-30 14:16:05 +0100

    Adding missed annotations to Grade class

--- src/main/java/org/rushland/plugin/entities/Grade.java
+++ src/main/java/org/rushland/plugin/entities/Grade.java
@@ -1,13 +1,17 @@
 package org.rushland.plugin.entities;
 
 import lombok.Getter;
+import org.rushland.api.interfaces.database.model.annotations.PrimaryQueryField;
+import org.rushland.api.interfaces.database.model.annotations.QueryField;
 
 /**
  * Managed by romain on 30/10/2014.
  */
 public class Grade {
+    @PrimaryQueryField
     private final int id;
     @Getter
+    @QueryField
     private final String prefix;
 
     public Grade() {
